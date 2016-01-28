3d00f7659ff967b3424f775b556bb13cf599bad5
Milton van de Sanden <salami350@live.nl>
2016-01-28 22:17:36 +0100

    init program

---
+++ Communication/src/communication/Communication.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package communication;
+
+import java.util.List;
+
+/**
+ *
+ * @author milton
+ */
+public class Communication
+{   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+    public List<String> readTextFile(String filePath)
+    {
+        return null;     
+    }
+    
+    public void writeTextFile(String filePath)
+    {
+        
+    }
+}
